ccce234d1570f93352c28f9eb9379477f1ea8e9a
hyunki <dhchung317@gmail.com>
2020-11-03 09:02:55 -0500

    yup

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const { ApolloServer, gql } = require('apollo-server')
 var Sequelize = require('sequelize');
-var sequelize
+var sequelize = new Sequelize('postgres://bwsbllqoiqyvtn:bd0e4466e4e41112f2b5f48a5b6b6f81d515c7f9da4ee62286cbc0af5d31051a@ec2-52-21-0-111.compute-1.amazonaws.com:5432/d4j89gtkj4rki0')
 
-var Location
+const Location = sequelize.define('Location', {
+    localeId: {
+        type: Sequelize.DataTypes.STRING
+    },
+    latitude: {
+        type: Sequelize.DataTypes.FLOAT,
+        allowNull: false
+    },
+    longitude: {
+        type: Sequelize.DataTypes.FLOAT,
+        allowNull: false
+    },
+    localeId: {
+        type: Sequelize.DataTypes.STRING
+    },
+})
+
 
 const typeDefs = gql`
 
@@ -63,48 +79,24 @@ const server = new ApolloServer({
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
     console.log(`🚀 Server ready at ${url}`);
-
-    sequelize = new Sequelize(`postgres://${url}/hyunki`);
-
-    Location = sequelize.define('Location', {
-        localeId: {
-            type: Sequelize.DataTypes.STRING
-        },
-        latitude: {
-            type: Sequelize.DataTypes.FLOAT,
-            allowNull: false
-        },
-        longitude: {
-            type: Sequelize.DataTypes.FLOAT,
-            allowNull: false
-        },
-        localeId: {
-            type: Sequelize.DataTypes.STRING
-        },
-    })
-
-    sequelize
-    .authenticate()
-    .then(function (err) {
-        console.log('Connection has been established successfully.');
-        //   User.sync()
-    })
-    .catch(function (err) {
-        console.log('Unable to connect to the database:', err);
-    });
-
-    (async () => {
-        await sequelize.sync({ force: true });
-    
-        // const place = await Location.create({ latitude: "1", longitiude: "0" });
-        // console.log("longitude:", place.longitude);
-    
-        // const places = await Location.findAll();
-        // console.log("All users:", JSON.stringify(places, null, 2));
-    })();
-
-
   })
 
+  sequelize
+  .authenticate()
+  .then(function (err) {
+      console.log('Connection has been established successfully.');
+      //   User.sync()
+  })
+  .catch(function (err) {
+      console.log('Unable to connect to the database:', err);
+  });
 
-
+  (async () => {
+      await sequelize.sync({ force: true });
+  
+      // const place = await Location.create({ latitude: "1", longitiude: "0" });
+      // console.log("longitude:", place.longitude);
+  
+      // const places = await Location.findAll();
+      // console.log("All users:", JSON.stringify(places, null, 2));
+  })();
